c6a2462a0b11b28c073ef1c9d103ff4d1097e413
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-06 21:22:05 +0400

    Update hw1task2_t1.py

--- hw1task2_t1.py
+++ hw1task2_t1.py
@@ -1,5 +1,5 @@
 
-import Task2
+import Task_2
 import unittest
 from unittest.mock import patch
 
@@ -8,9 +8,8 @@ class TestMainFunction(unittest.TestCase):
     @patch('builtins.input', side_effect=["150"])
     def test_main_input_calls(self, mock_input):
         #with self.assertRaises(SystemExit):
-        Task2.main()
+        Task_2.main()
         expected_calls = ["Deqiqe-> "]
         # Check if input was called with the expected arguments
         self.assertEqual(mock_input.call_args_list, [unittest.mock.call(arg) for arg in expected_calls])
-
 
