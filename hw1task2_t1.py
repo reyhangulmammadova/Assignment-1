b2b9923c23dd4e433db85b3577da26fe3b919d74
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-06 21:20:30 +0400

    Update hw1task2_t1.py

--- hw1task2_t1.py
+++ hw1task2_t1.py
@@ -1,1 +1,16 @@
 
+import Task2
+import unittest
+from unittest.mock import patch
+
+class TestMainFunction(unittest.TestCase):
+    
+    @patch('builtins.input', side_effect=["150"])
+    def test_main_input_calls(self, mock_input):
+        #with self.assertRaises(SystemExit):
+        Task2.main()
+        expected_calls = ["Deqiqe-> "]
+        # Check if input was called with the expected arguments
+        self.assertEqual(mock_input.call_args_list, [unittest.mock.call(arg) for arg in expected_calls])
+
+
