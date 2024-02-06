226c85eab45b85ab6053e2555e8d60de8a68fbc4
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-06 21:32:24 +0400

    Update hw1task2_t2.py

--- hw1task2_t2.py
+++ hw1task2_t2.py
@@ -1,21 +1,22 @@
-
 import Task_2
 import unittest
 from io import StringIO
 from unittest.mock import patch
-from parameterized import parameterized
 
 class TestMainFunction(unittest.TestCase):
-    @parameterized.expand([
-        (['150'], "150 deqiqe =  2 saat 30 deqiqedir\n")
-    ])
-   
-    def test_input_and_output(self, input_values, expected_output):
-        with patch('builtins.input', side_effect=lambda _: input_values.pop(0)):
-            if expected_output is not None:
-                with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                    Task_2.main()
-                self.assertEqual(mock_stdout.getvalue(), expected_output)
-            else:
-                with self.assertRaises(ValueError):
-                    Task_2.main()
+    def test_input_and_output(self):
+        input_values = ['150']
+        expected_output = "150 deqiqe =  2 saat 30 deqiqedir\n"
+        
+        with patch('builtins.input', side_effect=input_values), \
+             patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            Task_2.main()
+
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_invalid_input(self):
+        input_values = ['abc']
+        
+        with patch('builtins.input', side_effect=input_values), \
+             self.assertRaises(ValueError):
+            Task_2.main()
