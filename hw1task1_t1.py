239d4483ca68fb129e2c138e0b5d6082aa0819f0
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-07 14:07:15 +0400

    Update hw1task1_t1.py

--- hw1task1_t1.py
+++ hw1task1_t1.py
@@ -6,5 +6,5 @@ def test_output(capsys):
     captured = capsys.readouterr()
 
     # Check if the output matches the expected output
-    expected_output = f"Defterin qiymeti:  {0.5}  azn\nKarandasin qiymeti:  {0.2}  azn\n{12} defter ve {5} karandasin umumi qiymeti:  {7}  azn\n"
+    expected_output = f"Defterin qiymeti:  {0.5}  azn\nKarandasin qiymeti:  {0.2}  azn\n{12} defter ve {5} karandasin umumi qiymeti:  {7.0}  azn\n"
     assert captured.out == expected_output
