83d4c78c734263c8e22b8244a3d26b08937ff2d3
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-07 14:03:49 +0400

    Update hw1task2_t2.py

--- hw1task2_t2.py
+++ hw1task2_t2.py
@@ -4,19 +4,22 @@ from io import StringIO
 from unittest.mock import patch
 
 class TestMainFunction(unittest.TestCase):
-    def test_input_and_output(self):
-        input_values = ['150']
-        expected_output = "150 deqiqe =  2 saat 30 deqiqedir\n"
-        
-        with patch('builtins.input', side_effect=input_values), \
+    def _test_with_input(self, input_value, expected_output):
+        with patch('builtins.input', return_value=input_value), \
              patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             Task_2.main()
-
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
+    def test_input_150(self):
+        self._test_with_input('150', "150 deqiqe =  2 saat 30 deqiqedir\n")
+
+    def test_input_62(self):
+        self._test_with_input('62', "62 deqiqe =  1 saat 2 deqiqedir\n")
+
     def test_invalid_input(self):
-        input_values = ['abc']
-        
-        with patch('builtins.input', side_effect=input_values), \
+        with patch('builtins.input', return_value='abc'), \
              self.assertRaises(ValueError):
             Task_2.main()
+
+if __name__ == '__main__':
+    unittest.main()
