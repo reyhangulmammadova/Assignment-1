ba9cd51cc8ff916ed50a19176d979a88818fa796
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-02-10 01:43:35 +0400

    Update hw1task1_t1.py

--- hw1task1_t1.py
+++ hw1task1_t1.py
@@ -1,10 +1,10 @@
 import Task_1
 
 def test_output(capsys):
-        # Run the script to capture the output
+    # Run the script to capture the output
     Task_1.main()
     captured = capsys.readouterr()
 
     # Check if the output matches the expected output
-    expected_output = f"Defterin qiymeti:  {0.5}  azn\nKarandasin qiymeti:  {0.2}  azn\n{12} defter ve {5} karandasin umumi qiymeti:  {7.0}  azn\n"
+    expected_output = f"Notebook price: {0.5} azn\nPencil price: {0.2} azn\n{12} notebooks and {5} pencils total price: {7.0} azn\n"
     assert captured.out == expected_output
