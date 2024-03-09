13081a3dd79cc08753437c72481a46b1f1b91d1d
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:42:44 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -2,7 +2,8 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+Minute = int(input("Minute: "))
+  print(f"{Minute} minutes = {Minute//60} hours {Minute%60} minutes")
 
 if __name__ == "__main__":
   main()
