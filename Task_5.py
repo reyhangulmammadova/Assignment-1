748737e25611876a54cac9cae671cc7da109f7da
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:18:01 +0400

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -1,11 +1,14 @@
 # import libraries here
-
-
+from math import log
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+  print(f"{a} + {b} = {a+b}")
+  print(f"{a} - {b} = {a-b}")
+  print(f"{a} * {b} = {a*b}")
+  print(f"{a} % {b} = {a%b}")
+  print(f"{a} // {b} = {a//b}")
+  print(f"log10( {a} ) = {log(a,10):.2f}")
 
 if __name__ == "__main__":
   main()
