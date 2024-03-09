9a3948599b33a626c7d22b191d8ef3c8a0f5c2a2
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:36:35 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,7 +2,13 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+s1 = int(input("Side1: "))
+s2 = int(input("Side2: "))
+s3 = int(input("Side3: "))
+s4 = (s1 + s2 + s3)/ 2
+s_ucbucaq =(s4*(s4-s1)*(s4-s2)*(s4-s3))**0.5
+  print(f"Area of triangle: {s_ucbucaq:.2f}")
+
 
 if __name__ == "__main__":
   main()
