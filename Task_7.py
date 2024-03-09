a864c2916a3fbef142095f384b5ee3c18cace28c
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:28:42 +0400

    Update Task_7.py

--- Task_7.py
+++ Task_7.py
@@ -1,11 +1,15 @@
 # import libraries here
-
+from math import tan
+from math import pi
 
 def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+s = float(input("Length: "))
+n = int(input("Number of sides: "))
+area = (n*s**2)/(4*math.tan(math.pi/n))
+  print(f"Area: {area}")
 
 if __name__ == "__main__":
   main()
