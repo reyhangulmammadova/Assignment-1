fd30388b7e24013b8b266cadc0d910d77356a008
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:26:58 +0400

    Update Task_9.py

--- Task_9.py
+++ Task_9.py
@@ -2,7 +2,9 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+loaves = int(input("Number of loaves: "))
+  print(f"Total price: {loaves*3.49:.3f} dollars")
+  print(f"Discount: {loaves*3.49*0.6} dollars")
 
 if __name__ == "__main__":
   main()
