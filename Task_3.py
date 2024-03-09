07588c72117099b47af2939582e7f3de11fdcba7
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:44:25 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -2,7 +2,15 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+number = int(input("Enter 4 digit number: "))
+first = number//1000
+second = (number//100)%10
+third = (number//10)%10
+fourth = number%10
+  print(f"Digits: {number//1000} {(number//100)%10} {(number//10)%10} {number%10}")
+  print(f"Sum of digits: {first} + {second} + {third} + {fourth} = {first + second + third + fourth}")
+  print(f"Product of digits: {first} * {second} * {third} * {fourth} = { first * second * third * fourth}")
+  
 
 if __name__ == "__main__":
   main()
