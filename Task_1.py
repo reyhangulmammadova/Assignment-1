efa6a077d9d7222739c3de44187a646900acaa85
Reyhan <160207864+reyhangulmammadova@users.noreply.github.com>
2024-03-09 18:41:05 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,7 +2,10 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
-  pass
+  print("Notebook price: 0.5 azn")
+  print("Pencil price: 0.2 azn")
+  print(f"12 notebooks and 5 pencils total price: {0.5*12+0.2*5} azn")
+  
 
 if __name__ == "__main__":
   main()
